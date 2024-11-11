42faa0a4de6edfd6012f0c63f4218314a0da352e
Kevin65k <chavarrkevinalexander@gmail.com>
2024-11-11 17:47:53 -0600

    Se ha agregado la interfaz grafica con los controles y su codificacion.

--- Interfaces/ClientesForm.Designer.cs
+++ Interfaces/ClientesForm.Designer.cs
@@ -29,12 +29,138 @@ namespace Clave3_Grupo4.Interfaces
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtBilleteraVirtual = new System.Windows.Forms.TextBox();
+            this.txtDUI = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnObtenerClientes = new System.Windows.Forms.Button();
+            this.cmbTipoProducto = new System.Windows.Forms.ComboBox();
+            this.dataGridViewClientes = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(248, 59);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // txtBilleteraVirtual
+            // 
+            this.txtBilleteraVirtual.Location = new System.Drawing.Point(248, 254);
+            this.txtBilleteraVirtual.Name = "txtBilleteraVirtual";
+            this.txtBilleteraVirtual.Size = new System.Drawing.Size(100, 20);
+            this.txtBilleteraVirtual.TabIndex = 1;
+            // 
+            // txtDUI
+            // 
+            this.txtDUI.Location = new System.Drawing.Point(248, 180);
+            this.txtDUI.Name = "txtDUI";
+            this.txtDUI.Size = new System.Drawing.Size(100, 20);
+            this.txtDUI.TabIndex = 2;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(248, 118);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 3;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(447, 59);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(447, 177);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 5;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(447, 103);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 6;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnObtenerClientes
+            // 
+            this.btnObtenerClientes.Location = new System.Drawing.Point(447, 215);
+            this.btnObtenerClientes.Name = "btnObtenerClientes";
+            this.btnObtenerClientes.Size = new System.Drawing.Size(135, 23);
+            this.btnObtenerClientes.TabIndex = 7;
+            this.btnObtenerClientes.Text = "Obtener Clientes";
+            this.btnObtenerClientes.UseVisualStyleBackColor = true;
+            this.btnObtenerClientes.Click += new System.EventHandler(this.btnObtenerClientes_Click);
+            // 
+            // cmbTipoProducto
+            // 
+            this.cmbTipoProducto.FormattingEnabled = true;
+            this.cmbTipoProducto.Location = new System.Drawing.Point(237, 215);
+            this.cmbTipoProducto.Name = "cmbTipoProducto";
+            this.cmbTipoProducto.Size = new System.Drawing.Size(121, 21);
+            this.cmbTipoProducto.TabIndex = 8;
+            // 
+            // dataGridViewClientes
+            // 
+            this.dataGridViewClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewClientes.Location = new System.Drawing.Point(84, 288);
+            this.dataGridViewClientes.Name = "dataGridViewClientes";
+            this.dataGridViewClientes.Size = new System.Drawing.Size(608, 150);
+            this.dataGridViewClientes.TabIndex = 9;
+            this.dataGridViewClientes.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewClientes_CellClick);
+            // 
+            // ClientesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridViewClientes);
+            this.Controls.Add(this.cmbTipoProducto);
+            this.Controls.Add(this.btnObtenerClientes);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtDUI);
+            this.Controls.Add(this.txtBilleteraVirtual);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "ClientesForm";
             this.Text = "ClientesForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewClientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtBilleteraVirtual;
+        private System.Windows.Forms.TextBox txtDUI;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnObtenerClientes;
+        private System.Windows.Forms.ComboBox cmbTipoProducto;
+        private System.Windows.Forms.DataGridView dataGridViewClientes;
     }
 }
