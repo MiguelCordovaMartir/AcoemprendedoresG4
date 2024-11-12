63ecf267ab203a6d6c5a9a607b1ce523ad7e5c67
Susana Valeria Palacios Menjívar <valerypalacios6@gmail.com>
2024-11-12 10:21:26 -0600

    Se han agregados los controles para la interfaz grafica Empleados con su respectivo código

--- Interfaces/EmpleadosForm.Designer.cs
+++ Interfaces/EmpleadosForm.Designer.cs
@@ -29,12 +29,151 @@ namespace Clave3_Grupo4.Interfaces
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtDUI = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificarEmpleado = new System.Windows.Forms.Button();
+            this.btnEliminarEmpleado = new System.Windows.Forms.Button();
+            this.btnObtenerClientes = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscarEmpleado = new System.Windows.Forms.Button();
+            this.dataGridViewEmpleados = new System.Windows.Forms.DataGridView();
+            this.cmbRol = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpleados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(104, 71);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(104, 117);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 1;
+            // 
+            // txtDUI
+            // 
+            this.txtDUI.Location = new System.Drawing.Point(104, 163);
+            this.txtDUI.Name = "txtDUI";
+            this.txtDUI.Size = new System.Drawing.Size(100, 20);
+            this.txtDUI.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(263, 71);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnModificarEmpleado
+            // 
+            this.btnModificarEmpleado.Location = new System.Drawing.Point(263, 117);
+            this.btnModificarEmpleado.Name = "btnModificarEmpleado";
+            this.btnModificarEmpleado.Size = new System.Drawing.Size(75, 23);
+            this.btnModificarEmpleado.TabIndex = 5;
+            this.btnModificarEmpleado.Text = "Modificar";
+            this.btnModificarEmpleado.UseVisualStyleBackColor = true;
+            this.btnModificarEmpleado.Click += new System.EventHandler(this.btnModificarEmpleado_Click);
+            // 
+            // btnEliminarEmpleado
+            // 
+            this.btnEliminarEmpleado.Location = new System.Drawing.Point(263, 163);
+            this.btnEliminarEmpleado.Name = "btnEliminarEmpleado";
+            this.btnEliminarEmpleado.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminarEmpleado.TabIndex = 6;
+            this.btnEliminarEmpleado.Text = "Eliminar";
+            this.btnEliminarEmpleado.UseVisualStyleBackColor = true;
+            this.btnEliminarEmpleado.Click += new System.EventHandler(this.btnEliminarEmpleado_Click);
+            // 
+            // btnObtenerClientes
+            // 
+            this.btnObtenerClientes.Location = new System.Drawing.Point(263, 211);
+            this.btnObtenerClientes.Name = "btnObtenerClientes";
+            this.btnObtenerClientes.Size = new System.Drawing.Size(157, 23);
+            this.btnObtenerClientes.TabIndex = 7;
+            this.btnObtenerClientes.Text = "Cargar todos los clientes";
+            this.btnObtenerClientes.UseVisualStyleBackColor = true;
+            this.btnObtenerClientes.Click += new System.EventHandler(this.btnObtenerClientes_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(402, 36);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(100, 20);
+            this.txtBuscar.TabIndex = 8;
+            // 
+            // btnBuscarEmpleado
+            // 
+            this.btnBuscarEmpleado.Location = new System.Drawing.Point(523, 33);
+            this.btnBuscarEmpleado.Name = "btnBuscarEmpleado";
+            this.btnBuscarEmpleado.Size = new System.Drawing.Size(118, 23);
+            this.btnBuscarEmpleado.TabIndex = 9;
+            this.btnBuscarEmpleado.Text = "Buscar empleado";
+            this.btnBuscarEmpleado.UseVisualStyleBackColor = true;
+            this.btnBuscarEmpleado.Click += new System.EventHandler(this.btnBuscarEmpleado_Click);
+            // 
+            // dataGridViewEmpleados
+            // 
+            this.dataGridViewEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEmpleados.Location = new System.Drawing.Point(52, 266);
+            this.dataGridViewEmpleados.Name = "dataGridViewEmpleados";
+            this.dataGridViewEmpleados.Size = new System.Drawing.Size(589, 183);
+            this.dataGridViewEmpleados.TabIndex = 11;
+            this.dataGridViewEmpleados.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewEmpleados_CellClick);
+            // 
+            // cmbRol
+            // 
+            this.cmbRol.FormattingEnabled = true;
+            this.cmbRol.Location = new System.Drawing.Point(104, 211);
+            this.cmbRol.Name = "cmbRol";
+            this.cmbRol.Size = new System.Drawing.Size(121, 21);
+            this.cmbRol.TabIndex = 12;
+            // 
+            // EmpleadosForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbRol);
+            this.Controls.Add(this.dataGridViewEmpleados);
+            this.Controls.Add(this.btnBuscarEmpleado);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.btnObtenerClientes);
+            this.Controls.Add(this.btnEliminarEmpleado);
+            this.Controls.Add(this.btnModificarEmpleado);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtDUI);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "EmpleadosForm";
             this.Text = "EmpleadosForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpleados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtDUI;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificarEmpleado;
+        private System.Windows.Forms.Button btnEliminarEmpleado;
+        private System.Windows.Forms.Button btnObtenerClientes;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnBuscarEmpleado;
+        private System.Windows.Forms.DataGridView dataGridViewEmpleados;
+        private System.Windows.Forms.ComboBox cmbRol;
     }
 }
