92dacd4e63924f1aa19000d5866bcae86d614896
Susana Valeria Palacios Menjívar <valerypalacios6@gmail.com>
2024-11-12 10:47:56 -0600

    Se ha mejorado la interfaz grafica del formulario Empleados

--- Interfaces/EmpleadosForm.Designer.cs
+++ Interfaces/EmpleadosForm.Designer.cs
@@ -41,33 +41,38 @@ namespace Clave3_Grupo4.Interfaces
             this.dataGridViewEmpleados = new System.Windows.Forms.DataGridView();
             this.cmbRol = new System.Windows.Forms.ComboBox();
             this.btnExportarExcel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpleados)).BeginInit();
             this.SuspendLayout();
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(104, 71);
+            this.txtNombre.Location = new System.Drawing.Point(129, 71);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(100, 20);
             this.txtNombre.TabIndex = 0;
             // 
             // txtApellido
             // 
-            this.txtApellido.Location = new System.Drawing.Point(104, 117);
+            this.txtApellido.Location = new System.Drawing.Point(129, 117);
             this.txtApellido.Name = "txtApellido";
             this.txtApellido.Size = new System.Drawing.Size(100, 20);
             this.txtApellido.TabIndex = 1;
             // 
             // txtDUI
             // 
-            this.txtDUI.Location = new System.Drawing.Point(104, 163);
+            this.txtDUI.Location = new System.Drawing.Point(129, 163);
             this.txtDUI.Name = "txtDUI";
             this.txtDUI.Size = new System.Drawing.Size(100, 20);
             this.txtDUI.TabIndex = 2;
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(263, 71);
+            this.btnAgregar.Location = new System.Drawing.Point(288, 71);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(75, 23);
             this.btnAgregar.TabIndex = 4;
@@ -77,7 +82,7 @@ namespace Clave3_Grupo4.Interfaces
             // 
             // btnModificarEmpleado
             // 
-            this.btnModificarEmpleado.Location = new System.Drawing.Point(263, 117);
+            this.btnModificarEmpleado.Location = new System.Drawing.Point(288, 117);
             this.btnModificarEmpleado.Name = "btnModificarEmpleado";
             this.btnModificarEmpleado.Size = new System.Drawing.Size(75, 23);
             this.btnModificarEmpleado.TabIndex = 5;
@@ -87,7 +92,7 @@ namespace Clave3_Grupo4.Interfaces
             // 
             // btnEliminarEmpleado
             // 
-            this.btnEliminarEmpleado.Location = new System.Drawing.Point(263, 163);
+            this.btnEliminarEmpleado.Location = new System.Drawing.Point(288, 163);
             this.btnEliminarEmpleado.Name = "btnEliminarEmpleado";
             this.btnEliminarEmpleado.Size = new System.Drawing.Size(75, 23);
             this.btnEliminarEmpleado.TabIndex = 6;
@@ -97,24 +102,24 @@ namespace Clave3_Grupo4.Interfaces
             // 
             // btnObtenerClientes
             // 
-            this.btnObtenerClientes.Location = new System.Drawing.Point(263, 211);
+            this.btnObtenerClientes.Location = new System.Drawing.Point(288, 211);
             this.btnObtenerClientes.Name = "btnObtenerClientes";
             this.btnObtenerClientes.Size = new System.Drawing.Size(157, 23);
             this.btnObtenerClientes.TabIndex = 7;
-            this.btnObtenerClientes.Text = "Cargar todos los clientes";
+            this.btnObtenerClientes.Text = "Cargar todos los empleados";
             this.btnObtenerClientes.UseVisualStyleBackColor = true;
             this.btnObtenerClientes.Click += new System.EventHandler(this.btnObtenerClientes_Click);
             // 
             // txtBuscar
             // 
-            this.txtBuscar.Location = new System.Drawing.Point(402, 36);
+            this.txtBuscar.Location = new System.Drawing.Point(496, 98);
             this.txtBuscar.Name = "txtBuscar";
             this.txtBuscar.Size = new System.Drawing.Size(100, 20);
             this.txtBuscar.TabIndex = 8;
             // 
             // btnBuscarEmpleado
             // 
-            this.btnBuscarEmpleado.Location = new System.Drawing.Point(523, 33);
+            this.btnBuscarEmpleado.Location = new System.Drawing.Point(617, 95);
             this.btnBuscarEmpleado.Name = "btnBuscarEmpleado";
             this.btnBuscarEmpleado.Size = new System.Drawing.Size(118, 23);
             this.btnBuscarEmpleado.TabIndex = 9;
@@ -125,7 +130,7 @@ namespace Clave3_Grupo4.Interfaces
             // dataGridViewEmpleados
             // 
             this.dataGridViewEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewEmpleados.Location = new System.Drawing.Point(52, 266);
+            this.dataGridViewEmpleados.Location = new System.Drawing.Point(113, 255);
             this.dataGridViewEmpleados.Name = "dataGridViewEmpleados";
             this.dataGridViewEmpleados.Size = new System.Drawing.Size(589, 183);
             this.dataGridViewEmpleados.TabIndex = 11;
@@ -134,14 +139,14 @@ namespace Clave3_Grupo4.Interfaces
             // cmbRol
             // 
             this.cmbRol.FormattingEnabled = true;
-            this.cmbRol.Location = new System.Drawing.Point(104, 211);
+            this.cmbRol.Location = new System.Drawing.Point(155, 215);
             this.cmbRol.Name = "cmbRol";
             this.cmbRol.Size = new System.Drawing.Size(121, 21);
             this.cmbRol.TabIndex = 12;
             // 
             // btnExportarExcel
             // 
-            this.btnExportarExcel.Location = new System.Drawing.Point(523, 68);
+            this.btnExportarExcel.Location = new System.Drawing.Point(617, 130);
             this.btnExportarExcel.Name = "btnExportarExcel";
             this.btnExportarExcel.Size = new System.Drawing.Size(118, 23);
             this.btnExportarExcel.TabIndex = 13;
@@ -149,11 +154,67 @@ namespace Clave3_Grupo4.Interfaces
             this.btnExportarExcel.UseVisualStyleBackColor = true;
             this.btnExportarExcel.Click += new System.EventHandler(this.btnExportarExcel_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 15);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 15);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Apellido:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(54, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 15);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "DUI:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(53, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 15);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Elija el cargo:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(493, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(265, 15);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Buscar empleado por nombre o apellido";
+            // 
             // EmpleadosForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnExportarExcel);
             this.Controls.Add(this.cmbRol);
             this.Controls.Add(this.dataGridViewEmpleados);
@@ -188,5 +249,10 @@ namespace Clave3_Grupo4.Interfaces
         private System.Windows.Forms.DataGridView dataGridViewEmpleados;
         private System.Windows.Forms.ComboBox cmbRol;
         private System.Windows.Forms.Button btnExportarExcel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
